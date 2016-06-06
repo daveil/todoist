bce4e4b56aea3e63209fc6b413b10f0dbfccc31f
Dave Alaras <arroyo.daveil@gmail.com>
2016-06-06 20:33:18 +0800

    Use access token

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use \Curl\Curl;
-
 $curl = new Curl();
-//$curl->get('http://www.example.com/');
-//echo $curl->response;
-echo $_ENV['TODOIST_TOKEN'];
+$data = array(
+	'token'=>$_ENV['TODOIST_TOKEN'],
+	'sync_token'=>'*',
+	'resource_types'=>['items']
+	);
+$curl->post('https://todoist.com/API/v7/sync',$data);
+$resp =  json_encode($curl->response,true);
+echo $resp."\n";
 ?>
