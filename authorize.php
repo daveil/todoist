0c562eb11616688d75fd99c48313e896f19ae78e
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-06-20 12:06:43 +0800

    Implement checking on authorize

--- authorize.php
+++ authorize.php
@@ -1,8 +1,12 @@
 <?php	
-$url = 'https://todoist.com/oauth/access_token?';
-$url = 'client_id='.$_ENV['TODOIST_CLIENT_ID'].'&';
-$url = 'client_secret='.$_ENV['TODOIST_CLIENT_SECRET'].'&';
-$url = 'code='.$_GET['code'].'&';
-$url = 'redirect_uri=https://'. $_SERVER['HTTP_HOST'].'/index.php';
-header("Location: $url");
+if(isset($_GET['code'])&& isset($_GET['state'])){
+	if($_GET['state']=='ALPHA'){
+		$url = 'https://todoist.com/oauth/access_token?';
+		$url = 'client_id='.$_ENV['TODOIST_CLIENT_ID'].'&';
+		$url = 'client_secret='.$_ENV['TODOIST_CLIENT_SECRET'].'&';
+		$url = 'code='.$_GET['code'].'&';
+		$url = 'redirect_uri=https://'. $_SERVER['HTTP_HOST'].'/index.php';
+		header("Location: $url");
+	}
+}
 ?>
