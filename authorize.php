6428f119325cce5ff3c0ce04a60c3ae4a3c63a3a
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-06-20 11:56:26 +0800

    Add authorize.php

---
+++ authorize.php
@@ -0,0 +1,8 @@
+<?php	
+$url = 'https://todoist.com/oauth/access_token?';
+$url = 'client_id='.$_ENV['TODOIST_CLIENT_ID'].'&';
+$url = 'client_secret='.$_ENV['TODOIST_CLIENT_SECRET'].'&';
+$url = 'code='.$_GET['code'].'&';
+$url = 'redirect_uri=https://'. $_SERVER['HTTP_HOST'];
+header("Location: $url");
+?>
