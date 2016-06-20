42a6fcd627630ede72d211f139213bfd9ef0d321
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-06-20 12:06:27 +0800

    Adjust index.php to handle authorization

--- index.php
+++ index.php
@@ -1,14 +1,30 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+$input = file_get_contents("php://input");
 
-use \Curl\Curl;
-$curl = new Curl();
-$data = array(
-	'token'=>$_ENV['TODOIST_TOKEN'],
-	'sync_token'=>'*',
-	'resource_types'=>['items']
-	);
-$curl->post('https://todoist.com/API/v7/sync',$data);
-$resp =  json_encode($curl->response,true);
-echo $resp."\n";
+if($input){
+	$data = json_decode($input, true);	
+	echo '<pre>';
+	print_r($data);
+	file_put_contents("tokens.txt",$input);
+}else{
+	require __DIR__ . '/vendor/autoload.php';
+
+	use \Curl\Curl;
+	$curl = new Curl();
+	$data = array(
+		'token'=>$_ENV['TODOIST_TOKEN'],
+		'sync_token'=>'*',
+		'resource_types'=>['items']
+		);
+	$curl->post('https://todoist.com/API/v7/sync',$data);
+	$resp =  json_encode($curl->response,true);
+	$data = json_decode($resp);
+	if(isset($data['error_tag'])){
+		$url='https://todoist.com/oauth/authorize?';
+		$url='client_id='.$_ENV['TODOIST_CLIENT_ID'].'&';
+		$url='scope=data:read&';
+		$url='state=ALPHA';
+		echo '<a href="'.$url.'">AUTHORIZE</a>';
+	}
+}
 ?>
