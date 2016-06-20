b2db507ac45ad04b72b3022e8c7bd79e7ed40d97
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-06-20 12:37:19 +0800

    Decode true

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ if($input){
 		);
 	$curl->post('https://todoist.com/API/v7/sync',$data);
 	$resp =  json_encode($curl->response,true);
-	$data = json_decode($resp);
+	$data = json_decode($resp,true);
 	print_r($data);
 	if(isset($data['error_tag'])){
 		$url='https://todoist.com/oauth/authorize?';
