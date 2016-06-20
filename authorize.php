6028c4aff2eccd0eac6a7fe66419c020975543e6
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-06-20 12:42:43 +0800

    Update authorize and htaccess

--- authorize.php
+++ authorize.php
@@ -5,7 +5,7 @@ if(isset($_GET['code'])&& isset($_GET['state'])){
 		$url = 'client_id='.$_ENV['TODOIST_CLIENT_ID'].'&';
 		$url = 'client_secret='.$_ENV['TODOIST_CLIENT_SECRET'].'&';
 		$url = 'code='.$_GET['code'].'&';
-		$url = 'redirect_uri=https://'. $_SERVER['HTTP_HOST'].'/index.php';
+		$url = 'redirect_uri=https://'. $_SERVER['HTTP_HOST'];
 		header("Location: $url");
 	}
 }
