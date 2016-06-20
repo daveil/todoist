4ffc1d0ffc271a46c6cdced67fa88653c57c830d
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-06-20 17:19:48 +0800

    Loop on maker to send summary on evernote

--- ifttt.php
+++ ifttt.php
@@ -1,18 +1,33 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 use \Curl\Curl;
+$curl = new Curl();
 
 if(isset($_POST['title'])&&isset($_POST['content'])){
 	//IFTTT
 	$event = 'update_todoist';
 	$token = $_ENV['IFTTT_TOKEN'];
-	$curl = new Curl();
 	$curl->setHeader('Content-Type', 'application/json');
 	$data = array(
 					'value1'=>$_POST['title'],
 					'value2'=>$_POST['content'],
 				);
 	$curl->post('https://maker.ifttt.com/trigger/'.$event.'/with/key/'.$token,$data);
+}else if(isset($_GET['maker'])){
+	$time = str_replace('at',' ',$_GET['maker']);
+	$time = strtotime($time);
+	$summary_id = date('y-m-d',$time);
+	$summary = json_decode(file_get_contents('summary.txt'),true);
+	if(isset($summary[$summary_id])){
+		$summaries = $summary[$summary_id];
+		foreach($summaries as $file){
+			$data = json_decode(file_get_contents($file.'.txt'),true);
+			$post_data = array(
+				'title'=>$data['title'],
+				'content'=>implode(" <br/>",$data['content'])
+				);
+			$curl->post('https://'. $_SERVER['HTTP_HOST'].'/ifttt.php',$post_data);
+		}
+	}
 }
-
 ?>
