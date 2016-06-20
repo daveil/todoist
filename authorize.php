377d9b3761add28a47c48faf366ca215c7c11d00
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-06-20 11:58:46 +0800

    Redirect to index.php

--- authorize.php
+++ authorize.php
@@ -3,6 +3,6 @@ $url = 'https://todoist.com/oauth/access_token?';
 $url = 'client_id='.$_ENV['TODOIST_CLIENT_ID'].'&';
 $url = 'client_secret='.$_ENV['TODOIST_CLIENT_SECRET'].'&';
 $url = 'code='.$_GET['code'].'&';
-$url = 'redirect_uri=https://'. $_SERVER['HTTP_HOST'];
+$url = 'redirect_uri=https://'. $_SERVER['HTTP_HOST'].'/index.php';
 header("Location: $url");
 ?>
