ff2bb2f220800e9edb38a0bd3cd03ad81235c241
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-06-20 14:08:08 +0800

    Process item:completed to create new file

--- todoist.php
+++ todoist.php
@@ -1,12 +1,34 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+use \Curl\Curl;
+//Read input
 $input = file_get_contents("php://input");
+//Read and parse contents
 $contents = json_decode(file_get_contents("events.txt"),true);
 $data = json_decode($input,true);
 if(!is_array($contents)){
 	$contents = array();
 }
+//Add timestamp
 $data['epoch']=time();
 array_push($contents,$data);
+//Write new contents
 file_put_contents("events.txt",json_encode($contents));
+//Create daily summary for complete item
+if($data['event_name']=='item:completed'){
+	//Request for item details
+	$curl = new Curl();
+	$data = array(
+		'token'=>$_ENV['TODOIST_TOKEN'],
+		'item_id'=>$data['event_data']['id']
+		);
+	$curl->post('https://todoist.com/API/v7/get_item',$data);
+	$item =  json_decode($curl->response,true);
+	//Build file
+	$date =  date('m d, Y',$data['epoch']);
+	$title = $item['project']['name']. 'Daily Summary -'.$date.'txt';
+	$content =  "* $item['item']['content'] \n";
+	file_put_contents($title, $content, FILE_APPEND | LOCK_EX);
+}
 ?>
 
