dc9542cf2ecc88755ccb8ccccc478b1a66a50b38
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-06-20 15:25:46 +0800

    Add authorize_evernote

---
+++ authorize_evernote.php
@@ -0,0 +1,17 @@
+<?php
+//EVERNOTE
+
+//set this to false to use in production
+$sandbox = true;
+
+$oauth_handler = new \Evernote\Auth\OauthHandler($sandbox);
+
+$key      = $_ENV['EVERNOTE_KEY'];
+$secret   = $_ENV['EVERNOTE_SECRET'];
+$callback = 'https://'. $_SERVER['HTTP_HOST'].'/authorize_evernote.php';
+
+$oauth_data  = $oauth_handler->authorize($key, $secret, $callback);
+
+$oauth_token = $oauth_data['oauth_token'];
+print_r($oauth_data);
+?>
