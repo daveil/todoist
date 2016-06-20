5861161142085d503f10791abc7ef542dada1777
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-06-20 13:38:40 +0800

    Read write on events.txt

--- todoist.php
+++ todoist.php
@@ -2,6 +2,8 @@
 $data = file_get_contents("php://input");
 $events = json_decode($data, true);
 $data = json_encode($event, true);
-file_put_contents("events.txt",$data);
+$contents = file_get_contents("events.txt",$data);
+$contents .=  $data;
+file_put_contents("events.txt",$contents);
 ?>
 
