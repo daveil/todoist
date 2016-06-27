2857dcc6df3d495b063fbb881543e682a56c714e
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-06-27 13:17:51 +0800

    Test writing to events.txt

--- dropbox.php
+++ dropbox.php
@@ -3,6 +3,14 @@ require __DIR__ . '/vendor/autoload.php';
 use \Dropbox as dbx;
 // We can now use $accessToken to make API requests.
 $dbxClient = new dbx\Client($_ENV['DROPBOX_TOKEN'], "WTTF");
+
+$f = fopen("events.txt", "w+b");
+$fileMetadata = $dbxClient->getFile("/events.txt", $f);
+fclose($f);
+echo 'Download file...<br/>';
+print_r($fileMetadata);
+
+/* 
 $accountInfo = $dbxClient->getAccountInfo();
 echo '<pre>';
 print_r($accountInfo);
@@ -15,13 +23,9 @@ echo 'Upload file...<br/>';
 print_r($result);
 
 $folderMetadata = $dbxClient->getMetadataWithChildren("/");
-print_r($folderMetadata);
+print_r($folderMetadata); 
+*/
 
-$f = fopen("working-draft.txt", "w+b");
-$fileMetadata = $dbxClient->getFile("/working-draft.txt", $f);
-fclose($f);
-echo 'Download file...<br/>';
-print_r($fileMetadata);
 	
 
 
