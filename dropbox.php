9d8a6d88d5ec0285fe75ed21b0465c3288a1a1ca
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-06-27 13:08:30 +0800

    Debug

--- dropbox.php
+++ dropbox.php
@@ -4,12 +4,13 @@ use \Dropbox as dbx;
 // We can now use $accessToken to make API requests.
 $dbxClient = new dbx\Client($_ENV['DROPBOX_TOKEN'], "WTTF");
 $accountInfo = $dbxClient->getAccountInfo();
-
+echo '<pre>';
 print_r($accountInfo);
 
 $f = fopen("working-draft.txt", "rb");
 $result = $dbxClient->uploadFile("/working-draft.txt", dbx\WriteMode::add(), $f);
 fclose($f);
+echo 'Upload file...<br/>';
 print_r($result);
 
 $folderMetadata = $dbxClient->getMetadataWithChildren("/");
@@ -18,6 +19,7 @@ print_r($folderMetadata);
 $f = fopen("working-draft.txt", "w+b");
 $fileMetadata = $dbxClient->getFile("/working-draft.txt", $f);
 fclose($f);
+echo 'Download file...<br/>';
 print_r($fileMetadata);
 	
 
